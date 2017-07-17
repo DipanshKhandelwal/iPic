6a7222104cf78f94c318d3d1ee05e484b9e2001e
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-17 14:49:57 +0530

    initialized variables in acivity2

--- app/src/main/java/com/example/dipanshkhandelwal/ipic/Main2Activity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/ipic/Main2Activity.java
@@ -1,10 +1,19 @@
 package com.example.dipanshkhandelwal.ipic;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private static final int REQUEST_TAKE_PHOTO = 0;
+    private static final int REQUEST_SELECT_IMAGE = 1;
+    private Uri uriPhoto;
+
+    Button bTakePhoto;
+    Button bFromGallery;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

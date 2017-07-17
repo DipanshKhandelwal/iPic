7dea398c848af78efc9ec1a776b1087a27d43925
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-17 14:48:54 +0530

    Overrided onActivityResult method

--- app/src/main/java/com/example/dipanshkhandelwal/ipic/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/ipic/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dipanshkhandelwal.ipic;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,5 +29,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_SELECT_IMAGE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Checking the requestCode and resultCode to perform a specific task
+        if (requestCode == REQUEST_SELECT_IMAGE && resultCode == RESULT_OK){
+            // If image is selected successfully, set the image URI.
+            Uri imageUri = data.getData();
+            if (imageUri != null) {
+
+                ivImage.setImageURI(imageUri);
+            }
+        }
     }
 }

2900bdff405fc6b84c4e295aa0063fc96600eb62
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-17 14:53:52 +0530

    added method takePhoto

--- app/src/main/java/com/example/dipanshkhandelwal/ipic/Main2Activity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/ipic/Main2Activity.java
@@ -2,10 +2,15 @@ package com.example.dipanshkhandelwal.ipic;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -24,6 +29,21 @@ public class Main2Activity extends AppCompatActivity {
         bTakePhoto = (Button) findViewById(R.id.bTakePhoto);
         bFromGallery = (Button) findViewById(R.id.bFromGallery);
 
+    }
+
+    public void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            // Save the photo taken to a temporary file.
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            try {
+                File file = File.createTempFile("IMG_", ".jpg", storageDir);
+                uriPhoto = Uri.fromFile(file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uriPhoto);
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+            } catch (IOException e) {
+            }
+        }
     }
 
     public void selectImageInGallery() {

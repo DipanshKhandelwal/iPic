1f7ab86adfbc7a6ec0acdde7b3ef9cd898f7f915
dipanshkhandelwal <deepanshrocks438@gmail.com>
2017-07-17 14:40:04 +0530

    Added a few variables

--- app/src/main/java/com/example/dipanshkhandelwal/ipic/MainActivity.java
+++ app/src/main/java/com/example/dipanshkhandelwal/ipic/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.dipanshkhandelwal.ipic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_SELECT_IMAGE = 0;
+    private Button bSelectImage;
+    private ImageView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
